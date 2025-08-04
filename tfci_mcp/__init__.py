e002322bc6a0ff4f52c7557327ebec12cea57d19
ci671233 <rosci671233@gmail.com>
2025-08-04 15:59:19 +0900

    Update tfci-mcp version to 1.0.3

--- tfci_mcp/__init__.py
+++ tfci_mcp/__init__.py
@@ -6,7 +6,7 @@ TFCI MCP (Model Context Protocol) Package
 from .client import TFCIMCPClient
 from .server import TFCIMCPServer
 
-__version__ = "1.0.0"
+__version__ = "1.0.3"
 __author__ = "TFCI Team"
 __email__ = "rosci671233@gmail.com"
 
